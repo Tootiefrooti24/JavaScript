987787092e46abc6a1ca480cac7cbaeadc7cb309
Tootiefrooti24 <pshrutz24@gmail.com>
2026-03-18 17:05:54 +0000

    Array notes

---
+++ 04_Array/01_Arrays.js
@@ -0,0 +1,9 @@
+//Array Object enables storing a collection of multiple items under a single variable name
+//Javascript arrays are resizable and can contain a mix of different datatypes
+//Zero based indexing 
+//Javascript array-copy operations create shallow copies
+//A shallow copy of an object is a copy whose properties share the same references (point to the same underlying values) as those of the source object from which the copy was made.
+//A deep copy of an object is a copy whose properties do not share the same references (point to the same underlying values) as those of the source object from which the copy was made.
+
+const myArr=[0,1,2,3,4,5]
+console.log(myArr[2]);
